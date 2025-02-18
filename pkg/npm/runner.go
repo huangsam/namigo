11e9c2f7b32fae1822ac778655001a528018998e
Samuel Huang <samhuang91@gmail.com>
2025-02-17 22:45:15 -0800

    Migrate NPM logic to pkg

---
+++ pkg/npm/runner.go
@@ -0,0 +1,48 @@
+package npm
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/huangsam/namigo/internal/util"
+)
+
+func SearchForMatch(name string) {
+	// Setup document query
+	client := &http.Client{Timeout: 5 * time.Second}
+	builder := func() (*http.Request, error) {
+		params := url.Values{"q": []string{"hello"}}
+		url := url.URL{
+			Scheme:   "https",
+			Host:     "www.npmjs.com",
+			Path:     "search",
+			RawQuery: params.Encode(),
+		}
+		return http.NewRequest("GET", url.String(), nil)
+	}
+	pipe := util.NewDocumentPipeline(client, builder)
+	doc, err := pipe.Execute()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Run document query
+	doc.Find("main section").Each(func(i int, section *goquery.Selection) {
+		pkg, ok := section.Find("div.bea55649 a").Attr("href")
+		if ok {
+			fmt.Printf("pkg: %s\n", pkg)
+		}
+
+		match := section.Find("div.bea55649 span#pkg-list-exact-match").Text()
+		fmt.Printf("match: %s\n", match)
+
+		description := section.Find("p").Text()
+		fmt.Printf("description: %s\n", description)
+
+		fmt.Println()
+	})
+}
