d4a9f42f671750a2946b0a594b0af0607e5271de
Samuel Huang <samhuang91@gmail.com>
2025-02-23 04:14:34 -0800

    Add tests for pypi builder

---
+++ pkg/search/pypi/builder_test.go
@@ -0,0 +1,46 @@
+package pypi
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestAPIList(t *testing.T) {
+	builder := APIList()
+	req, err := builder()
+	if err != nil {
+		t.Fatalf("APIList() error = %v", err)
+	}
+
+	expectedURL := "https://pypi.org/simple/"
+	if req.URL.String() != expectedURL {
+		t.Errorf("APIList() URL = %v, want %v", req.URL.String(), expectedURL)
+	}
+
+	expectedHeader := "application/vnd.pypi.simple.v1+json"
+	if req.Header.Get("Accept") != expectedHeader {
+		t.Errorf("APIList() Accept header = %v, want %v", req.Header.Get("Accept"), expectedHeader)
+	}
+
+	if req.Method != http.MethodGet {
+		t.Errorf("APIList() method = %v, want %v", req.Method, http.MethodGet)
+	}
+}
+
+func TestAPIDetail(t *testing.T) {
+	pkg := "example-package"
+	builder := APIDetail(pkg)
+	req, err := builder()
+	if err != nil {
+		t.Fatalf("APIDetail() error = %v", err)
+	}
+
+	expectedURL := "https://pypi.org/pypi/" + pkg + "/json"
+	if req.URL.String() != expectedURL {
+		t.Errorf("APIDetail() URL = %v, want %v", req.URL.String(), expectedURL)
+	}
+
+	if req.Method != http.MethodGet {
+		t.Errorf("APIDetail() method = %v, want %v", req.Method, http.MethodGet)
+	}
+}
