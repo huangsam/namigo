dbb94691dd086b703ff718acc9ef492e2a2099b2
Samuel Huang <samhuang91@gmail.com>
2025-03-19 00:05:49 -0700

    Refactor prompt template and CLI utils

--- cmd/namigo/main.go
+++ cmd/namigo/main.go
@@ -9,6 +9,30 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+// checkSizeFlag checks for valid size flag.
+func checkSizeFlag(ctx *cli.Context, i int) error {
+	if i <= 0 {
+		return fmt.Errorf("size %d is invalid", i)
+	}
+	return nil
+}
+
+// checkLengthFlag checks for valid length flag.
+func checkLengthFlag(ctx *cli.Context, i int) error {
+	if i <= 0 {
+		return fmt.Errorf("length %d is invalid", i)
+	}
+	return nil
+}
+
+// checkFormatFlag checks for valid format flag.
+func checkFormatFlag(ctx *cli.Context, s string) error {
+	if s != model.TextValue && s != model.JSONValue {
+		return fmt.Errorf("format %s is invalid", s)
+	}
+	return nil
+}
+
 func main() {
 	app := &cli.App{
 		Name:  "namigo",
--- pkg/generate/generate.go
+++ pkg/generate/generate.go
@@ -2,9 +2,26 @@
 package generate
 
 import (
+	_ "embed"
 	"strings"
 	"text/template"
 )
+
+// promptName is a name for the template below.
+const promptName = "name_brainstorm_with_json"
+
+// promptData is the input for the prompt template.
+type promptData struct {
+	Purpose      string // Project purpose
+	Theme        string // Project theme
+	Demographics string // Target demographics
+	Interests    string // Target interests
+	MaxSize      int    // Maximum number of names to generate
+	MaxLength    int    // Maximum length of each name
+}
+
+//go:embed project.template
+var promptTemplate string
 
 // GeneratePrompt generates the prompt for AI chatbots.
 func GeneratePrompt(purpose, theme, demographics, interests string, size, length int) (string, error) {
