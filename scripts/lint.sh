b8cc0867c76df379c53bf005ac0e926f936e6cb5
Samuel Huang <samhuang91@gmail.com>
2025-05-16 21:08:14 -0700

    Add comments to developer scripts

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 set -eu
 
-golangci-lint run
+# default: Run golangci-lint for all packages.
+# fix: Run golangci-lint and fix issues.
+# format: Run golangci-lint fmt to format the code.
+mode="${1:-default}"
+
+case "$mode" in
+    default)
+        golangci-lint run ;;
+    fix)
+        golangci-lint run --fix ;;
+    format)
+        golangci-lint fmt ;;
+    *)
+        echo "Invalid mode '$mode' detected" && exit 1 ;;
+esac
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 set -eu
 
+# default: Run tests for all packages with caching.
+# bench: Run benchmarks for all packages.
+# cover: Run tests and report coverage for all packages.
 mode="${1:-default}"
 
 selector=(
-    "github.com/huangsam/namigo/pkg/..."
-    "github.com/huangsam/namigo/internal/..."
+    "github.com/huangsam/go-trial/pkg/..."
+    "github.com/huangsam/go-trial/internal/..."
 )
 
 case "$mode" in
-    "default")
+    default)
         go test "${selector[@]}" ;;
-    "bench")
+    bench)
         go test -bench=. "${selector[@]}" ;;
-    "cover")
+    cover)
         go test -cover "${selector[@]}" ;;
     *)
         echo "Invalid mode '$mode' detected" && exit 1 ;;
